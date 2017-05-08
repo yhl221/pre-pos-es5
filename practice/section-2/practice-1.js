ddf625d9f493c9b4acf2e74d9a4741c66a178f53
yhl221 <1727538058@qq.com>
2017-05-08 12:57:30 +0800

    modify section-2

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,25 +1,26 @@
 'use strict';
 
 function countSameElements(collection) {
-     var result=[];
-    for(var i=0;i<collection.length;i++){
-       var object=isExist(collection[i],result);
-       if(object){
-         object.count++;
-       }else{
-         result.push({key:collection[i],count:1});
-       }
+  var result = [];
+
+  for (var i = 0; i < collection.length; i++) {
+    var object = isExist(collection[i], result);
+    if (object) {
+      object.count++;
+    } else {
+      result.push({key: collection[i], count: 1});
     }
+  }
 
   return result;
 }
 
 
-function isExist(element,array) {
-  for(var i=0;i<array.length;i++){
-    if(element === array[i].key){
+function isExist(element, array) {
+
+  for (var i = 0; i < array.length; i++) {
+    if (element === array[i].key) {
       return array[i];
     }
   }
-  return false;
 }
--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,50 +1,41 @@
 'use strict';
 
 function countSameElements(collection) {
-    var result = [];
-    for (var i = 0; i < collection.length; i++) {
-        var newArray = collection[i].split("-");
-        var newObject = judge(newArray);
-        var object = isExist(newArray[0], result);
-        /*if (newArray.length === 1) {
-            if (object) {
-                object.count++
-            } else {
-                result.push({key: collection[i], count: 1});
-            }
-        } else {
-            if (object) {
-                object.count += parseInt(newArray[1]);
-            } else {
-                result.push({key: newArray[0], count: parseInt(newArray[1])});
-            }
-        }*/
-        if(object){
-            object.count+=newObject.number;
-        }else{
-            result.push({key: newObject.key, count: newObject.number});
-        }
+  var result = [];
+
+  for (var i = 0; i < collection.length; i++) {
+    var newArray = collection[i].split("-");
+    var newObject = judge(newArray);
+    var object = isExist(newArray[0], result);
+    if (object) {
+      object.count += newObject.number;
+    } else {
+      result.push({key: newObject.key, count: newObject.number});
     }
-    return result;
+  }
+
+  return result;
 }
 
 
 function isExist(element, array) {
-    for (var i = 0; i < array.length; i++) {
-        if (element === array[i].key) {
-            return array[i];
-        }
+
+  for (var i = 0; i < array.length; i++) {
+    if (element === array[i].key) {
+      return array[i];
     }
-    return false;
+  }
 }
 
-function judge(array) {
-    var object={};
-    if(array.length===1){
-        object={key:array[0],number:1};
-    }else{
-        object={key:array[0],number:parseInt(array[1])}
-    }
 
-    return object;
+function judge(array) {
+
+  var object = {};
+  if (array.length === 1) {
+    object = {key: array[0], number: 1};
+  } else {
+    object = {key: array[0], number: parseInt(array[1])}
+  }
+
+  return object;
 }
